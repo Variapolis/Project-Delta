474db4382146983c5063aea2a632fbc18310018c
Vari <tmarksinbox@gmail.com>
2022-11-14 17:47:07 +0000

    feat(main-menu): add server creation ui

---
+++ Assets/CreateServerPanel.cs
@@ -0,0 +1,42 @@
+using System;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UniRx;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class CreateServerPanel : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Button createServerButton;
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TMP_Dropdown maxPlayers;
+    [SerializeField] private Toggle joinInProgress; // TODO: Add JIP Functionality
+
+    private void Start()
+    {
+        createServerButton.onClick
+            .AsObservable()
+            .Where(_ => nameInput.text != string.Empty)
+            .Subscribe(_ => CreateRoom())
+            .AddTo(gameObject);
+
+        nameInput.onEndEdit
+            .AsObservable()
+            .Where(t => t == string.Empty)
+            .Subscribe()
+            .AddTo(gameObject);
+    }
+
+    private void CreateRoom()
+    {
+        var roomOptions = new RoomOptions
+        {
+            IsVisible = true,
+            MaxPlayers = byte.Parse(maxPlayers.options[maxPlayers.value].text)
+        };
+        PhotonNetwork.CreateRoom(nameInput.text, roomOptions);
+    }
+}
