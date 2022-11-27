22dfd0a9420fec4336e982f786db2973b015fec7
Vari <tmarksinbox@gmail.com>
2022-11-27 01:08:27 +0000

    feat(online-play): add singletons to MultiplayerSceneInstaller.cs

--- Assets/MultiplayerSceneInstaller.cs
+++ Assets/MultiplayerSceneInstaller.cs
@@ -5,11 +5,14 @@ public class MultiplayerSceneInstaller : MonoInstaller
 {
     [SerializeField] private CameraHolderController cameraHolder;
     [SerializeField] private CrosshairController crosshair;
-    
+
     public override void InstallBindings()
     {
         Container.Bind<CameraHolderController>().FromInstance(cameraHolder).AsSingle().NonLazy();
         Container.Bind<CrosshairController>().FromInstance(crosshair).AsSingle().NonLazy();
-        
+        Container.Bind<SpawnPointList>().AsSingle();
+        Container.Bind<MouseVisibilityController>().AsSingle().NonLazy();
+        Container.Bind<PlayerList>().AsSingle().NonLazy();
+        Container.Bind<Registrar>().AsSingle().NonLazy();
     }
 }
