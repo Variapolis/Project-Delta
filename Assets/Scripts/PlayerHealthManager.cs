165b4c6e0902b66f85c7a3bb6aa73302363a4c43
Vari <tmarksinbox@gmail.com>
2022-11-28 01:28:53 +0000

    refactor(player-health): utilize player health in ClientPlayerModel.cs

--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -2,6 +2,7 @@ using System;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
+using UniRx;
 using UnityEngine;
 using Zenject;
 
@@ -9,13 +10,21 @@ public class PlayerHealthManager : MonoBehaviour, IDamageable
 {
     [Inject] private PhotonView _photonView;
     [Inject] private ClientPlayerModel _playerModel;
-    [SerializeField] private float health = 100f;
+    // [SerializeField] private ReactiveProperty<float> health = new(100);
+    [SerializeField] private float startingHealth = 100f;
+    private float Health
+    {
+        get => _playerModel.PlayerHealth.Value;
+        set => _playerModel.PlayerHealth.Value = value;
+    }
+
+    private void Start() => Health = startingHealth;
 
     public void Damage(float damage, Player attacker, IDamageable.DamageType damageType = IDamageable.DamageType.Hit)
     {
         if (!_photonView.IsMine) return;
-        health -= damage;
-        if (health <= 0) KillPlayer(attacker);
+        Health -= damage;
+        if (Health <= 0) KillPlayer(attacker);
 
         if (damageType == IDamageable.DamageType.Stun) StunPlayer();
         if (damageType == IDamageable.DamageType.Blind) BlindPlayer();
@@ -23,18 +32,13 @@ public class PlayerHealthManager : MonoBehaviour, IDamageable
 
     private void KillPlayer(Player attacker)
     {
-        if (!Equals(attacker, _photonView.Owner)) attacker.AddScore(1);
+        if (!Equals(attacker.GetPhotonTeam(), _photonView.Owner.GetPhotonTeam())) attacker.AddScore(1);
         if (_photonView.IsMine) _playerModel.IsAlive.Value = false;
+        Health = 0;
         PhotonNetwork.Destroy(gameObject);
     }
 
-    private void BlindPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
+    private void BlindPlayer() => throw new NotImplementedException();
 
-    private void StunPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
+    private void StunPlayer() => throw new NotImplementedException();
 }
